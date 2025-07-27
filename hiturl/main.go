f9c40323514feed5b1dae018276125cd3faed278
youngshin <youngshinwaw@gmail.com>
2025-07-27 11:15:03 +0900

    feat: goroutine hiturl 적용

--- hiturl/main.go
+++ hiturl/main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
-// var errRequestFailed = errors.New("request failed")
-
-func main() {
-	channel := make(chan string)
-
-	go sampleFunction("John Doe", channel)
-	go sampleFunction("Jane Smith", channel)
-	person := <-channel
-	fmt.Println("Received from channel:", person)
-	person = <-channel
-	fmt.Println("Received from channel:", person)
+type URLResult struct {
+	url    string
+	result string
 }
 
-func sampleFunction(person string, c chan string) {
-	c <- person + " is perpect"
+func main() {
+	channel := make(chan URLResult)
+
+	urls := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.twitter.com",
+		"https://www.linkedin.com",
+		"https://www.github.com",
+	}
+
+	for _, url := range urls {
+		go hitURl(url, channel)
+	}
+
+	for range urls {
+		result := <-channel
+		if result.result == "ok" {
+			println(result.url + " is reachable")
+		} else {
+			println(result.url + " is not reachable")
+		}
+	}
+}
+
+func hitURl(url string, c chan<- URLResult) {
+
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		c <- URLResult{url: url, result: "failed"}
+	}
+
+	if resp.StatusCode == 200 {
+		c <- URLResult{url: url, result: "ok"}
+	}
 }
